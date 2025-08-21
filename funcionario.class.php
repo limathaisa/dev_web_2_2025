b405ceec4d1ed9d15f538989e0b154a154669458
thaisa <limathaisa1204@gmail.com>
2025-08-21 14:49:57 -0300

    Update funcionario.class.php

--- funcionario.class.php
+++ funcionario.class.php
@@ -1,10 +1,10 @@
-<?php
+<?PHP
     include("class_pai.class.php");
     class Funcionario extends ClassePai {
         public $nome;
         public $telefone;
         public $salario;
-
+        const NOME_ARQUIVO = "../db/funcionario.txt";
         public function __construct($id, $nome, $salario, $telefone) {
             parent::__construct($id, "../db/funcionario.txt");
             $this->nome = $nome;
@@ -16,7 +16,7 @@
         {
             return $this->id.self::SEPARADOR.$this->nome.self::SEPARADOR.$this->salario.self::SEPARADOR.$this->telefone;
         }    
-        static public function listar() {
+        static public function listar($filtroNome) {
             $arquivo = fopen("../db/funcionario.txt", "r");
             $retorno = [];
             while(!feof($arquivo)){
@@ -24,9 +24,14 @@
                 if(empty($linha))
                     continue;
                 $dados = explode(self::SEPARADOR, $linha);
-                array_push($retorno, new Funcionario($dados[0], $dados[1], $dados[2], $dados[3]));
+                if(str_contains($dados[1], $filtroNome)){
+                    array_push($retorno, new Funcionario($dados[0], $dados[1], $dados[2], $dados[3]));
+                }
+                
             }
             return $retorno;
         }
+
+        
     }
 ?>
