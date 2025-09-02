bb28df332b00bf658976910d8caa7ebce7ad4b90
thaisa <limathaisa1204@gmail.com>
2025-09-02 14:40:32 -0300

    Update tabela_funcionario.php

--- tabela_funcionario.php
+++ tabela_funcionario.php
@@ -1,19 +1,21 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-      <label for="">Filtro: </label><input type="text" name="filtro">
-      <button type="submit"></button>
-    </form>
-    <?php
-    include("../service/produto.service.php");
-    $filtro = isset($_POST["filtro"]) ? $_POST["filtro"] : "";
-    listarFuncionario($filtro);
-    ?>
+<form method="post">
+<label>Nome:</label><input name="filtro"/>
+<button>Filtrar</button>
+</form>
+<?php
+include("../../service/funcionario.service.php");
+$filtro = isset($_POST["filtro"])?$_POST["filtro"]:"";
+listarFuncionario($filtro);
+?>
 </body>
 </html>
