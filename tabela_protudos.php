7069d1e1629bd997f6faf3534bbb34825beddf3c
thaisa <limathaisa1204@gmail.com>
2025-09-02 14:32:07 -0300

    Create tabela_protudos.php

---
+++ tabela_protudos.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<form method="post">
+<label>Nome:</label><input name="filtro"/>
+<button>Filtrar</button>
+</form>
+<?php
+include("../../service/produto.service.php");
+$filtro = isset($_POST["filtro"])?$_POST["filtro"]:"";
+listarProduto($filtro);
+?>
+</body>
+</html>
