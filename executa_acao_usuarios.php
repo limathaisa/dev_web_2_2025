ed95ec71609c8d19728087bf0d1402339a56eb2f
thaisa <limathaisa1204@gmail.com>
2025-09-09 14:54:53 -0300

    Update executa_acao_usuarios.php

--- executa_acao_usuarios.php
+++ executa_acao_usuarios.php
@@ -1,23 +1,30 @@
-<?
-include("../../service/usuario.service.php");
-$acao = $_POST['acao'];
-$nome = isset($_POST['nome'])?$_POST['nome']:null;
-$email = isset($_POST['email'])?$_POST['email']:null;
-$senha = isset($_POST['senha'])?$_POST['senha']:null;
-$id = isset($_POST['id'])?$_POST['id']:null;
-if($acao=="cadastrar") {
-cadastrarUsuario($nome, $email, $senha);
-echo "Cadastrado com sucesso";
-}
-else if($acao=="alterar") {
-alterarUsuario($nome, $email, $senha);
-echo "Alterado com sucesso";
-}
-else if($acao=="remover") {
-removerUsuario($id);
-echo "Removido com sucesso";
-}
-else {
-echo "Ação inválida";
-}
+<?php
+include("../../service/venda.service.php");
+  $acao = $_POST['acao'];
+  $cliente = isset($_POST['cliente'])?$_POST['cliente']:null;
+  $func = isset($_POST['funcionario'])?$_POST['funcionario']:null;
+  $produtos = isset($_POST['produtos_hidden'])?explode(',', $_POST['produtos_hidden']):null;
+  $qtd = isset($_POST['qtd'])?explode(',', $_POST['qtd']):null;
+  $data = isset($_POST['data'])?$_POST['data']:null;
+  $pagamento = isset($_POST['pagamento'])?$_POST['pagamento']:null;
+  $desconto = isset($_POST['desconto'])?$_POST['desconto']:null;
+  $id = isset($_POST['id'])?$_POST['id']:null;
+
+  if($acao=="cadastrar") {
+    $valorTotal = calcularTotal($produtos, $qtd, $desconto);
+    cadastrarVenda($cliente, $func, $produtos, $qtd, $valorTotal, $data, $pagamento, $desconto);
+    echo "Cadastrado com sucesso";
+  }
+  else if($acao=="alterar") {
+    $valorTotal = calcularTotal($produtos, $qtd, $desconto);
+    alterarVenda($cliente, $func, $produtos, $qtd, $valorTotal, $data, $pagamento, $desconto);
+    echo "Alterado com sucesso";
+  }
+  else if($acao=="remover") {
+    removerVenda($id);
+    echo "Removido com sucesso";
+  }
+  else {
+    echo "Ação inválida";
+  }
 ?>
