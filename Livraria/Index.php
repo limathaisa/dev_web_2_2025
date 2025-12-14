835599ea30c3d382fc7c9b33307b973e14e32f86
thaisa <limathaisa1204@gmail.com>
2025-12-14 20:59:50 -0300

    Add Index.php for handling API requests

---
+++ Livraria/Index.php
@@ -0,0 +1,37 @@
+<?php
+  require_once __DIR__ . "/controller/GenericController.php";
+  require_once __DIR__ . "/controller/LivroController.php";
+  require_once __DIR__ . "/controller/UsuarioController.php";
+  $metodo = $_SERVER['REQUEST_METHOD'];
+  $modulo = @$_GET['modulo'];
+  $controller = null;
+  $mysqli = new mysqli("localhost", "root", "", "biblioteca");
+  // localhost/index.php?modulo=livro
+  switch($modulo) {
+    case "usuario":
+        $controller = new UsuarioController($mysqli);
+        break;
+    case "livro":
+        $controller = new LivroController($mysqli);
+        break;
+    default: 
+        return json_encode("{erro: true, mensagem: 'Módulo Inválido'}");
+  }
+  $dadosRecebidos = json_decode(file_get_contents("php://input", true));
+  switch($metodo) {
+    case "POST":
+        $controller->cadastrar($dadosRecebidos);
+        echo json_encode(["erro"=>false, "mensagem"=> "Cadastrado com sucesso!"]);
+        exit;
+    case "GET":
+        echo json_encode($controller->listar($dadosRecebidos));
+        exit;
+    case "PUT":
+        echo json_encode($controller->alterar($dadosRecebidos));
+        exit;
+    case "DELETE":
+        $controller->remover($dadosRecebidos);
+        echo json_encode(["erro"=>false, "mensagem"=> "Removido com sucesso!"]);
+        exit;
+  }
+?>
